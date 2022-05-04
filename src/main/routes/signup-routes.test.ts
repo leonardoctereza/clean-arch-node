ab56d8899463a8971bb2c17f073998cc93ec6c48
Leonardo Tereza <l.tereza@swordhealth.com>
2022-05-04 20:11:57 -0300

    feat: add route for signup

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('SignUp Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app).post('/api/signup')
+      .send({
+        name: 'Leonardo',
+        email: 'leonardo@email.com',
+        password: '1234',
+        passwordConfirmation: '1234'
+      })
+      .expect(200)
+  })
+})
