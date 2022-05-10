baf38117b90b8bb7c4419fa7614856344dfa21ab
Leonardo Tereza <l.tereza@swordhealth.com>
2022-05-10 18:05:21 -0300

    feat: add factory to route

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,10 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
 
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'],
   coverageDirectory: 'coverage',
 
   testEnvironment: 'node',
