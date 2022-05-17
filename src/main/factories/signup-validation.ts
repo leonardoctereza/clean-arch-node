a1eb709ec3c83835669063e62fdf3099066b3a49
Leonardo Tereza <l.tereza@swordhealth.com>
2022-05-17 20:48:04 -0300

    refactor: move EmailValidation to composite

--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -3,12 +3,16 @@ import { RequiredFieldValidation } from '../../presentation/helpers/validators/r
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
+import { EmailValidation } from '../../presentation/helpers/validators/email-validation'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 
 export const makeSignUpValidation = (): ValidationComposite => {
+  const emailValidatorAdapter = new EmailValidatorAdapter()
   const validations: Validation[] = []
   for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
     validations.push(new RequiredFieldValidation(field))
   }
   validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
+  validations.push(new EmailValidation('email', emailValidatorAdapter))
   return new ValidationComposite(validations)
 }
