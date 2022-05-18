4fa901971dc1152ccd47e1d3460ea09ffb2b5075
Leonardo Tereza <l.tereza@swordhealth.com>
2022-05-17 21:23:34 -0300

    test: validation composite

---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,38 @@
+import { MissingParamError } from '../../errors'
+import { Validation } from './validation'
+import { ValidationComposite } from './validation-composite'
+
+interface SutTypes {
+  sut: ValidationComposite
+  validationStubs: Validation[]
+}
+
+const makeFakeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+const makeSut = (): SutTypes => {
+  const validationStubs = [makeFakeValidation(), makeFakeValidation()]
+  const sut = new ValidationComposite(validationStubs)
+  return { sut, validationStubs }
+}
+
+describe('ValidationComposite', () => {
+  test('Should return an error if any validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new MissingParamError('field'))
+
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  test('Should not return if validations pass', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
+})
