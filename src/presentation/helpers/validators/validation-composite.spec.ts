4b1f59b500d44b6bbb95b5dbffaf4fd949031c5a
Leonardo Tereza <l.tereza@swordhealth.com>
2022-05-17 21:25:07 -0300

    test: ensure validationComposite returns the first error

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -30,6 +30,23 @@ describe('ValidationComposite', () => {
     expect(error).toEqual(new MissingParamError('field'))
   })
 
+  test('Should return an error if any validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
+
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  test('Should return the first error if more then one validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error())
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
+
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toEqual(new Error())
+  })
+
   test('Should not return if validations pass', () => {
     const { sut } = makeSut()
     const error = sut.validate({ field: 'any_value' })
