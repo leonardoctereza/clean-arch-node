0eb29f66eb84d31cb711a5303612d3a54598dcbc
Leonardo Tereza <l.tereza@swordhealth.com>
2022-05-19 20:13:11 -0300

    feat: implements compare to bcryptAdapter

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -4,6 +4,9 @@ import { BcryptAdapter } from './bcrypt-adapter'
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
     return Promise.resolve('hashed')
+  },
+  async compare (): Promise<boolean> {
+    return true
   }
 }))
 
@@ -14,23 +17,43 @@ const makeSut = (): BcryptAdapter => {
 }
 
 describe('Bcrypt Adapter', () => {
-  test('Should call bcrypt with correct values', async () => {
+  test('Should call hash with correct values', async () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.hash('any_value')
     expect(hashSpy).toBeCalledWith('any_value', salt)
   })
 
-  test('Should a hash on success', async () => {
+  test('Should return valid hash on hash success', async () => {
     const sut = makeSut()
     const hash = await sut.hash('any_value')
     expect(hash).toBe('hashed')
   })
 
-  test('Should throw if bcrypt throws', async () => {
+  test('Should throw if hash throws', async () => {
     const sut = makeSut()
     jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => { throw new Error() })
     const promise = sut.hash('any_value')
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call compare with correct values', async () => {
+    const sut = makeSut()
+    const compareSpy = jest.spyOn(bcrypt, 'compare')
+    await sut.compare('any_value', 'any_hash')
+    expect(compareSpy).toBeCalledWith('any_value', 'any_hash')
+  })
+
+  test('Should return true if compare succeeds', async () => {
+    const sut = makeSut()
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toEqual(true)
+  })
+
+  test('Should throw if hash throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.compare('any_value', 'any_token')
     await expect(promise).rejects.toThrow()
   })
 })
--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,10 +1,15 @@
 import { Hasher } from '../../data/protocols/criptography/hasher'
 import bcrypt from 'bcrypt'
+import { HashComparer } from '../../data/protocols/criptography/hash-comparer'
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
   private readonly salt: number
   constructor (salt: number) {
     this.salt = salt
+  }
+
+  async compare (value: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(value, hash)
   }
 
   async hash (value: string): Promise<string> {
