8fa802a75fa308f7e14b6665991fe37a9fac3de1
Leonardo Tereza <l.tereza@swordhealth.com>
2022-05-25 20:11:56 -0300

    feat: add login route

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -1,8 +1,22 @@
-// import { LoginController } from "../../../presentation/controllers/login/login";
-// import { Controller } from "../../../presentation/protocols";
-// import { makeLoginValidation } from "./login-validation";
+import envs from '../../config/env'
+import { makeLoginValidation } from './login-validation-factory'
+import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
+import { Controller } from '../../../presentation/protocols'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
+import { LoginController } from '../../../presentation/controllers/login/login-controller'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-repository'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-repository'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
 
-// const makeLoginController = (): Controller {
-//   const validation = makeLoginValidation()
-//   return new LoginController(, validation)
-// }
+export const makeLoginController = (): Controller => {
+  const salt = 12
+  const validation = makeLoginValidation()
+  const accountMongoRepository = new AccountMongoRepository()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter(envs.jwtSecret)
+  const dbAuthetication = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+  const logMongoRepository = new LogMongoRepository()
+  const loginController = new LoginController(dbAuthetication, validation)
+  return new LogControllerDecorator(loginController, logMongoRepository)
+}
---
+++ src/main/routes/login-routes.test.ts
@@ -0,0 +1,50 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
+
+describe('Login Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app).post('/api/signup')
+        .send({
+          name: 'Leonardo',
+          email: 'leonardo@email.com',
+          password: '1234',
+          passwordConfirmation: '1234'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return a accessToken on success', async () => {
+      const password = await hash('1234', 12)
+      await accountCollection.insertOne({
+        name: 'Leonardo',
+        email: 'leonardo@email.com',
+        password
+      })
+      await request(app).post('/api/login')
+        .send({
+          email: 'leonardo@email.com',
+          password: '1234'
+        })
+        .expect(200)
+    })
+  })
+})
