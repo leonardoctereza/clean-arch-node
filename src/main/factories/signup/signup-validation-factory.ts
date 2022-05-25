1357abad686e7b335e996a1ada2727caa73a8192
Leonardo Tereza <l.tereza@swordhealth.com>
2022-05-25 19:00:46 -0300

    refactor: move EmailValidatorAdapter to main layer

--- src/main/factories/signup/signup-validation-factory.ts
+++ src/main/factories/signup/signup-validation-factory.ts
@@ -1,6 +1,6 @@
 
 import { Validation } from '../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+import { EmailValidatorAdapter } from '../../adapters/validators/email-validator-adapter'
 import { EmailValidation, ValidationComposite, RequiredFieldValidation, CompareFieldsValidation } from '../../../presentation/helpers/validators'
 
 export const makeSignUpValidation = (): ValidationComposite => {
